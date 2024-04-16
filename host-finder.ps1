d244b1aee0da05edd0b12b7ff810314ea4a16699
Bruno Albuquerque <albuquerquebruno2020@gmail.com>
2024-04-16 15:25:36 -0300

    Script that search hosts in a network

--- host-finder.ps1
+++ host-finder.ps1
@@ -1,24 +1,46 @@
+# Prompt the user to enter the range of hostnames
 [int]$range = Read-Host "Enter the range of hostnames"
+
+# Prompt the user to enter the hostname constant
 [string]$constantName = Read-Host "Enter the hostname constant"
 
-New-Item -Path "hosts.txt" -ItemType "file"
-if (!(Test-Path "hosts.txt")) {
-    New-Item -Path "hosts$range.txt" -ItemType "file" -Force SilentContiue
-}
+# Create a new file named "hosts.txt" forcefully, if it doesn't exist already
+New-Item -Path "hosts.txt" -ItemType "file" -Force SilentContinue
 
-for ([int]$i = 1; $i -le $range; $i++) {
-    [string]$hostname = "$constantName$i"
-    Write-Host "Pinging $realHost..."
-    [int]$result = Test-Connection -ComputerName $hostname -Count 1 -Quiet
+# Iterate over the range of hostnames
+for ($i = 1; $i -le $range; $i++) {
+    # Construct the hostname by appending the constant name with the current index
+    $hostname = "$constantName$i"
 
-    [string]$ip = [System.Net.Dns]::GetHostAddresses($hostname).IPAddressToString
-    Write-Host "O endereço IP é: $ip"
+    # Display a message indicating pinging of the hostname
+    Write-Host "Pinging $hostname..."
 
+    # Ping the hostname and store the result
+    $result = Test-Connection -ComputerName $hostname -Count 1 -Quiet
+
+    # Get the IP address corresponding to the hostname
+    $ip = [System.Net.Dns]::GetHostAddresses($hostname).IPAddressToString
+
+    # Display the IP address of the hostname
+    Write-Host "$hostname IP address is: $ip"
+
+    # Check if the hostname is reachable
     if ($result) {
+        # Display a message indicating the hostname is reachable
         Write-Host "$hostname is reachable."
-        Add-Content -Path "hosts.txt" -Value "$hostname -" -Force
+
+        # Append the hostname and its IP address to the "hosts.txt" file
+        Add-Content -Path "hosts.txt" -Value "$hostname($ip)" -Force
     } else {
+        # Display a message indicating no response for the hostname
         Write-Host "No response for $hostname."
     }
-    Get-Content hosts.txt
+}
+
+# Read the content of the "hosts.txt" file into the $computers variable
+$computers = Get-Content -Path hosts.txt
+
+# Iterate over and display each line in the "hosts.txt" file
+foreach ($computer in $computers) {
+    Write-Host "Computer host: $computers"
 }
